c96e3008658df594464232921f01e26d3297dbdb
Ssolar5 <109261817+Ssolar5@users.noreply.github.com>
2025-05-20 22:01:48 -0500

    feat: Implement `install_resources` functions

--- install_resources.py
+++ install_resources.py
@@ -1,29 +1,43 @@
 from configparser import SectionProxy
+from lxml import html
 from pycurl import Curl
 import requests
 import tarfile
 from tempfile import TemporaryFile
-from lxml import html
+from zipfile import ZipFile
 
 
 URL_PLSDB_META = "https://ccb-microbe.cs.uni-saarland.de/plsdb2025/download_meta.tar.gz"
 URL_MOBILEOG_DB = "https://mobileogdb.flsi.cloud.vt.edu/entries/database_download"
+URL_TNCENTRAL_DB = "https://tncentral.ncc.unesp.br/api/download_blast/nc/tn_in_is"
 
 
-# TODO test
-def install_plsdb_meta(to_dir: str):
-    response = requests.get(URL_PLSDB_META, stream=True)
-    if response.status_code != 200:
-        raise ConnectionError(
-            f"Request to {URL_PLSDB_META} returned code {response.status_code}"
-        )
+def _curl_download(fileobj, url: str):
+    cl = Curl()
+    cl.setopt(cl.URL, url)
+    cl.setopt(cl.WRITEDATA, fileobj)
+    cl.perform()
+    cl.close()
 
+
+def _download_targz_and_extract(to_dir: str, url: str):
     with TemporaryFile() as f_download:
-        f_download.write(response.raw.read())
+        _curl_download(f_download, url)
 
         with tarfile.open(fileobj=f_download) as f_tar:
             f_tar.extractall(path=to_dir)
 
+
+def _download_zip_and_extract(to_dir: str, url: str):
+    with TemporaryFile() as f_download:
+        _curl_download(f_download, url)
+
+        with ZipFile(f_download) as f_zip:
+            f_zip.extractall(path=to_dir)
+
+
+def install_plsdb_meta(to_dir: str):
+    _download_targz_and_extract(to_dir, URL_PLSDB_META)
 
 def install_mobileog_db(to_dir: str):
     targz_url = None
@@ -36,20 +50,11 @@ def install_mobileog_db(to_dir: str):
         targz_url = tree_page.xpath(
             "(/html/body/main/ul/li[1]/div/div[@class='btn-group'])[5]/a/@href"
         )[0]
-
-    with TemporaryFile() as f_download:
-        cl = Curl()
-        cl.setopt(cl.URL, targz_url)
-        cl.setopt(cl.WRITEDATA, f_download)
-        cl.perform()
-        cl.close()
-
-        with tarfile.open(fileobj=f_download) as f_tar:
-            f_tar.extractall(path=to_dir)
+    _download_zip_and_extract(to_dir, targz_url)
 
 
 def install_tncentral_db(to_dir: str):
-    pass
+    _download_zip_and_extract(to_dir, URL_TNCENTRAL_DB)
 
 
 def install_resources(config: SectionProxy):
@@ -58,7 +63,6 @@ def install_resources(config: SectionProxy):
     print("Installed PLSDB meta.")
 
     print("Installing mobileOG-db...")
-    # FIXME reference instead of hard-code path
     install_mobileog_db("mobileOG-db-beatrix-1.6")
     print("Installed mobileOG-db.")
 
